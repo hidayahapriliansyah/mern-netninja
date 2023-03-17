d6e95a9a9348ade6ff42fad47255655a5127bc18
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-17 22:06:31 +0700

    04-mongodb-atlas-and-mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const workoutsRoutes = require('./routes/workouts');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to db and listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // middleware
 app.use(express.json());
 
 // routes
 app.use('/api/workouts', workoutsRoutes);
-
-// listen for request
-app.listen(process.env.PORT, () => {
-  console.log(`App listen on port ${process.env.PORT}`);
-});
-
