544e49b567c20b94149d2439ca72ffad327e5dfc
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-18 10:00:08 +0700

    06-controllers-part-1

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
-const Workout = require('../models/workoutModel');
+const workoutController = require('../controllers/workoutController');
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({ message: "Get all workouts" });
-});
+router.get('/', workoutController.getAllWorkouts);
 
 // GET a single workouts
-router.get('/:id', (req, res) => {
-  res.json({ message: "Get a single workout" });
-});
+router.get('/:id', workoutController.getWorkout);
 
 // POST a workout
-router.post('/', async (req, res) => {
-  const { title, reps, load } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(201).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/', workoutController.createWorkout);
 
 // DELETE a workout
 router.delete('/', (req, res) => {
