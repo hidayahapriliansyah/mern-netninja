7b2b6bad17687f3a1930b3198be647fc90e60e78
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-23 21:55:55 +0700

    04-email-and-password-validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -16,10 +17,20 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function(email, password) {
-  const exist = await this.findOne({ email });
+  if (!email || !password) {
+    throw new Error('All field must be filled');
+  }
 
+  const exist = await this.findOne({ email });
   if (exist) {
     throw new Error('Email already in use');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error('Email is not valid');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error('Password is not strong enoug');
   }
 
   const salt = await bcrypt.genSalt(10);
