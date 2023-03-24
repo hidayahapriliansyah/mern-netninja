7e120ef23436fa07a2240d2065f6e6daf56dd628
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-24 16:38:55 +0700

    13-setting-the-initial-auth-status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+
   const logout = useLogout();
   const handleClick = () => {
     logout();
@@ -14,13 +17,16 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
+          { user
+              ? (<div>
+                <span>{user.email}</span>
+                <button onClick={handleClick}>Logout</button>
+              </div>)
+              : (<div>
+                  <Link to="/login">Login</Link>
+                  <Link to="/signup">Signup</Link>
+                </div>)
+          }
         </nav>
       </div>
     </header>
